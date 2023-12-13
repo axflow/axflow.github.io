654f1cbae8033418276af58135ef6667f753d3da
Ben Reinhart <benjreinhart@gmail.com>
2023-12-12 16:27:44 -0800

    Remove site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://axflow.github.io",
-  base: "/axflow.github.io",
   integrations: [tailwind()],
 });
