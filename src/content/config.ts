b448f3186ba53adafaf23b18621758a12ceaec13
Nicholas Charriere <nicholascharriere@gmail.com>
2023-12-21 13:21:31 -0800

    Add cover images to OG cards, as well as twitter OG tags for posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,20 @@
 import { z, defineCollection } from "astro:content";
 // The blog posts on the site.
 const postsCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
+  type: "content",
+
+  schema: ({ image }) =>
+    z.object({
       title: z.string(),
       description: z.string(),
-      authors: z.array(z.enum(['ben', 'nicholas'])),
+      authors: z.array(z.enum(["ben", "nicholas"])),
       tags: z.array(z.string()),
       published: z.date().optional(),
-    })
+      cover: image().refine((img) => img.width >= 1080, {
+        message: "Cover image must be at least 1080 pixels wide!",
+      }),
+    }),
 });
-
 
 export const collections = {
   posts: postsCollection,
