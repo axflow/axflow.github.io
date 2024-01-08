faaa6891b7506b028633102b82907a3d26572acc
Nicholas Charriere <nicholascharriere@gmail.com>
2024-01-07 19:05:11 -0800

    Force dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,7 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     container: {
