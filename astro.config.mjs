ff99eb6f6ef64a6ac900ea815969330e876e5cff
Nicholas Charriere <nicholascharriere@gmail.com>
2024-02-14 13:51:30 -0800

    Add calendly shortcuts

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,15 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: "https://axflow.github.io",
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  redirects: {
+    "/book": {
+      status: 302,
+      destination: "https://calendly.com/nichochar/30min",
+    },
+    "/demo": {
+      status: 302,
+      destination: "https://calendly.com/nichochar/axflow-customer-demo",
+    },
+  },
 });
